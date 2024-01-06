e3d18fdb51a3869da5c5da67e48309034b9cedc1
kangruixiang <thisispiggy@gmail.com>
2024-01-06 15:16:42 -0500

    removed ios android build

--- android/app/src/main/java/awesome/code/recorder/io/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package awesome.code.recorder.io;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- build/_app/immutable/nodes/0.eINsNRa5.js
+++
@@ -1,1 +0,0 @@
-import{m as e,_ as m}from"../chunks/0.nlpGJCrJ.js";export{e as component,m as universal};
